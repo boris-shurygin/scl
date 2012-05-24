90e2787efa5802ed842cbbd7540c32345e126aba
borisshr@gmail.com <borisshr@gmail.com@9e9d830c-d880-8c3c-5d35-b3da52a54e29>
2012-05-24 07:30:21 +0000

    Added host.h
    git-svn-id: https://scl.googlecode.com/svn/trunk@8 9e9d830c-d880-8c3c-5d35-b3da52a54e29

---
+++ sources/Tools/Utils/host.h
@@ -0,0 +1,21 @@
+/**
+ * @file: host.h 
+ * Assertion related routines of Utils library for SCL (Simple Compiler)
+ * @defgroup Asserts Assertions
+ * @brief Assertion routines/macros
+ * @ingroup Utils
+ */
+/*
+ * Copyright (C) 2012  Boris Shurygin
+ */
+#pragma once
+#ifndef UTILS_HOST_H
+#define UTILS_HOST_H
+
+/* System headers */
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#endif /* UTILS_HOST_H */
