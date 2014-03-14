a4868fae86e8853534716764830a38690ee0428d
Boris Shurygin <borisshr@gmail.com>
2014-03-14 16:46:42 +0400

    Further development of NFA

--- sources/regexp/fa.cpp
+++ sources/regexp/fa.cpp
@@ -5,7 +5,29 @@
 #include "regexp_iface.h"
 
 namespace RegExp
-{
+{ 
+    /** Construct empty NFA subgraph*/
+    SubNFA::SubNFA( NFA& parent): nfa( parent)
+    {
+        begin_ = parent.newState();
+        end_ = parent.newState();
+    }
+    
+    /** Construct empty NF\A subgraph with given start and stop states */
+    SubNFA::SubNFA( NFA& parent, State b, State e): 
+        nfa( parent), begin_( b), end_( e)
+    {
+    }
+        
+
+    /** Construct NFA subgraph accepting one character */
+    SubNFA::SubNFA( NFA& parent, Character ch):nfa( parent)
+    {
+        begin_ = parent.newState();
+        end_ = parent.newState();
+        parent.addTransition( begin_, end_, ch);
+    }
+        
     /**
      * Printing routine of NFA, prints NFA as a graph in Graphviz's DOT format
      */
@@ -47,18 +69,55 @@ namespace RegExp
         }
     }
 
+    /** Make new NFA subgraph that corresponds to result of concatenation operation of the given subgraphs */
+    SubNFA NFA::concat( const SubNFA& left, const SubNFA& right)
+    {
+        SubNFA res( *this, left.begin(), right.end());
+        addEpsTransition( left.end(), right.begin());
+        return res;
+    }
+
+    /** Make new NFA subgraph that corresponds to result of alternation operation on the given subgraphs */
+    SubNFA NFA::alter( const SubNFA& left, const SubNFA& right)
+    {
+        SubNFA res( *this);
+        
+        addEpsTransition( res.begin(), left.begin());
+        addEpsTransition( res.begin(), right.begin());
+        
+        addEpsTransition( left.end(), res.end());
+        addEpsTransition( right.end(), res.end());
+        
+        return res;
+    }
+
+    /** Make new NFA subgraph that corresponds to Kleene closure operation result on the given subgraph */
+    SubNFA NFA::star( const SubNFA& snfa)
+    {
+        SubNFA res( *this);
+        
+        addEpsTransition( res.begin(), snfa.begin() );
+        addEpsTransition( res.begin(), res.end() );
+        addEpsTransition( snfa.end(), res.end() );
+        addEpsTransition( snfa.end(), snfa.begin() );
+
+        return res;
+    }
+
 
     /** Test finite automaton operation */
     bool uTestFA( UnitTest *utest)
     {
+        NFA nfa;
+
         // Should recognize letter 'a'
-        NFA nfa_a('a'); 
+        SubNFA nfa_a( nfa, 'a'); 
         
         // Should recognize letter 'b'
-        NFA nfa_b('b'); 
+        SubNFA nfa_b( nfa, 'b'); 
         
         // Should recognize letter 'c'
-        NFA nfa_c('c'); 
+        SubNFA nfa_c( nfa, 'c'); 
 #if 0        
         // Should recognize word 'ab'
         NFA nfa_concat = nfa_a + nfa_b;
--- sources/regexp/fa.h
+++ sources/regexp/fa.h
@@ -62,6 +62,41 @@ namespace RegExp
         State dst_;
     };
 
+    /* Predeclaration */
+    class NFA;
+
+    /**
+     * Representation of NFA subgraph for composing NFA using Thompson's algorithm
+     */
+    class SubNFA: public Printable<SubNFA>
+    {
+    public:
+        SubNFA( NFA& parent); /**< Construct empty NFA subgraph*/
+        SubNFA( NFA& nfa, Character ch); /**< Construct NFA subgraph accepting one character */
+        SubNFA( NFA& parent, State b, State e); /**< Construct empty NFA subgraph with given start and stop states */
+        
+        /** Text printing routine */
+        void toStream( std::ostream &stm) const 
+        {
+            stm << "start " << begin_ << "end " << end_;
+        }
+        
+        inline State begin() const { return begin_;} /**< Return the initial state of this NFA subgraph */
+        inline State end() const { return end_;}  /**< Return the accepting state of this NFA subgraph */
+        inline NFA& parentNFA() const { return nfa;} /**< Get the parent NFA */
+        
+        /**
+         * Build concatenation of two SubNFAs
+         */
+        inline SubNFA operator *();
+
+    private:
+        /* Internal state representation */
+        State begin_;
+        State end_;
+        NFA& nfa;
+    };
+
     /**
      * Non-deterministic finite automaton representation
      */
@@ -76,10 +111,27 @@ namespace RegExp
         virtual ~NFA(); /**< Destructor */
         void toStream( std::ostream &stm) const; /**< Text printing routine */
         
+        /** Create transition from one state to another with condition of transition */
         void addTransition( State s, State d, Character c);
+        
+        /** Create epsilon transition from one state to another */
         void addEpsTransition( State s, State d);
         
+        /** Add a new state to the NFA */
         State newState() { return state_index++;}
+
+        /** Make new NFA subgraph that corresponds to result of concatenation operation of the given subgraphs */
+        SubNFA concat( const SubNFA& left, const SubNFA& right);
+
+        /** Make new NFA subgraph that corresponds to result of alternation operation on the given subgraphs */
+        SubNFA alter( const SubNFA& left, const SubNFA& right);
+
+        /** Make new NFA subgraph that corresponds to Kleene closure operation result on the given subgraph */
+        SubNFA star( const SubNFA& snfa);
+
+        /** Turns transitions into table */
+        void buildTable();
+
     private:
         NFA( const NFA& orig){}; /**< Copy constructor closed */
         NFA& operator=( const NFA& orig){ return *this;}; /**< Assignment closed */
@@ -91,6 +143,32 @@ namespace RegExp
         std::set<State> accepting_states;
         Transitions transitions;
     };
+
+    /**
+     * Build concatenation of two SubNFAs
+     */
+    inline SubNFA operator+( const SubNFA& left, const SubNFA& right)
+    {
+        RE_ASSERTD( areEqP( &left.parentNFA(), &right.parentNFA()));
+        return left.parentNFA().concat( left, right);
+    }
+
+    /**
+     * Build alternation of two SubNFAs
+     */
+    inline SubNFA operator|( const SubNFA& left, const SubNFA& right)
+    {
+        RE_ASSERTD( areEqP( &left.parentNFA(), &right.parentNFA()));
+        return left.parentNFA().alter( left, right);
+    }
+
+    /**
+     * Build Kleene closure of SubNFA
+     */
+    inline SubNFA SubNFA::operator*()
+    {
+        return parentNFA().star( *this);
+    }
 
     /** Test finite automaton operation */
     bool uTestFA( UnitTest *utest);
